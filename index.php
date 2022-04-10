848264522d22659d1272d9bd218dbe396532115e
Nassef Khachiche <nassefkhachiche1@gmail.com>
2022-04-11 00:58:09 +0200

    added some more info

--- index.php
+++ index.php
@@ -14,23 +14,49 @@
 <body>
     <div class="container">
         <ul>
+            <li><a href="#"><img src="" alt=""></a></li>
             <li><a href="#">Home</a></li>
             <li><a href="#">Portfolio</a></li>
             <li><a href="#">Contact</a></li>
             <li><a href="#">About</a></li>
-            <div class="menu-btn-burger">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Hamburger_icon_white.svg/1024px-Hamburger_icon_white.svg.png" 
-                style="width: 10%; height: 10%; display: flex; justify-content: right;">
-            </div>
         </ul>
         <div class="intro">
             <div class="left">
-                <h1>Hi, I'm Nassef Khachiche </h1>
+                <h1>Nassef Khachiche</h1><br>
             </div>
             <div class="right">
                 <img src="https://vbcnotarissen.nl/wp-content/uploads/2015/02/placeholder-hi.png">
             </div>
         </div>
+        <div class="row">
+        <div class="info">
+            <div>
+                <h3>Goals</h3>
+                <p>I want to become a succesfull software developer</p>
+            </div>
+            <div>
+                <img src="https://wikiimg.tojsiabtv.com/wikipedia/commons/thumb/c/c5/Target_Corporation_logo_%28vector%29.svg/1200px-Target_Corporation_logo_%28vector%29.svg.png" alt="">
+            </div>
+        </div>
+        <div class="info">
+            <div>
+                <h3>Goals</h3>
+                <p>I want to learn Javascript</p>
+            </div>
+            <div>
+                <img src="img/Javascript.png" alt="">
+            </div>
+        </div>
+        <div class="info">
+            <div>
+                <h3>Achievements</h3>
+                <p>I'm currently working on a <br> Laravel project</p>
+            </div>
+            <!-- <div>
+                <img src="img/Java.png" alt="">
+            </div> -->
+        </div>
+    </div>
     </div>
 </body>
 
